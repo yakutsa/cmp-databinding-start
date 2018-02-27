133ec3b61a2a034dc3020f41cee357f561886a13
Savas Yakut <savas.yakut@gmail.com>
2018-02-27 23:11:32 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type: 'server', name: 'Testserver', content: 'just a test!'}];
+
+
+
+}
